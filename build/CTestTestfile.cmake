c9ffc7293420de0007ebe4c1d3515a5e0c1dba5a
tetsutetsu <s152044qs@s.chibakoudai.jp>
2018-01-15 22:55:20 +0900

    frist commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/togo/catkin_ws/src
+# Build directory: /home/togo/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(mypkg)
